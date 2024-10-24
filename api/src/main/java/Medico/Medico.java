a22cad59e50431304a3a887aa410e214bbd5085e
João Emanuel Pessoa do Nascimento <joao.emanuel.pn@gmail.com>
2024-10-24 19:48:10 -0300

    Add files via upload

---
+++ api/src/main/java/Medico/Medico.java
@@ -0,0 +1,38 @@
+package Medico;
+
+import jakarta.persistence.*;
+import lombok.*;
+import med.voll.api.endereco.Endereco;
+import med.voll.api.medico.DadosCadastroMedico;
+import med.voll.api.medico.Especialidade;
+
+@Getter
+@NoArgsConstructor
+@AllArgsConstructor
+@EqualsAndHashCode(of = "id")
+@Table(name = "medicos")
+@Entity(name = "Medico")
+public class Medico {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private long id;
+    private String nome;
+    private String email;
+    private String crm;
+
+    @Enumerated(EnumType.STRING)
+    private Especialidade especialidade;
+
+    @Embedded
+    private Endereco endereco;
+
+
+    public Medico(DadosCadastroMedico dados) {
+        this.nome = dados.nome();
+        this.email = dados.email();
+        this.crm = dados.crm();
+        this.especialidade = dados.especialidade();
+        this.endereco = new Endereco(dados.endereco());
+    }
+}
---
+++ api/src/main/java/med/voll/api/medico/MedicoRepository.java
@@ -0,0 +1,9 @@
+package med.voll.api.medico;
+
+import Medico.Medico;
+import org.springframework.data.jpa.repository.JpaRepository;
+
+
+public interface MedicoRepository extends JpaRepository<Medico,Long> {
+
+}
